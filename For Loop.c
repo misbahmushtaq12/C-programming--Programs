8c9fcba9dce1c951d3a7a73db91ecfa0479386d9
misbahmushtaq12 <114083173+misbahmushtaq12@users.noreply.github.com>
2022-09-21 23:51:36 +0530

    Add files via upload

---
+++ For Loop.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+
+{
+int i;
+
+
+for(i=0; i<11; i++)
+{
+printf("%d",i);
+
+
+
+printf("\n");
+} return 0;
+
+
+}
+
---
+++ LOOP 1-10.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+
+{
+int i;
+
+
+for(i=0; i<11; i++)
+{
+printf("%d",i);
+
+
+
+printf("\n");
+} return 0;
+
+}
+\\*
